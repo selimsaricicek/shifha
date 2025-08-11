ea7f9e238ce804d9148ff4679251ed6d7cbc15f1
Mehmet Pektaş <mehmetpektasss18@gmail.com>
2025-08-11 21:03:25 +0300

    Remove tracked files and update .gitignore - exclude development folders and config files

--- shifha backend/src/config/app.js
+++
@@ -1,8 +0,0 @@
-// ...existing code...
-const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config();
-const app = express();
-app.use(express.json());
-// ...existing code...
-module.exports = app;
--- shifha backend/src/utils/logger.js
+++
@@ -1,8 +0,0 @@
-/**
- * Basit loglama fonksiyonu
- * @param {string} msg - Loglanacak mesaj
- */
-const logger = (msg) => {
-  console.log(msg);
-};
-module.exports = logger;
